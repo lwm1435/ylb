93e0a383499218bf3d6f4c762cabea289bbae838
lwm1435 <lwm1435@163.com>
2022-01-07 18:32:29 +0800

    /receive/task/kq/query 向快钱查询充值订单结果并作出处理（定时任务）

--- micr-pay/src/main/java/com/lwm/pay/controller/KuaiQianController.java
+++ micr-pay/src/main/java/com/lwm/pay/controller/KuaiQianController.java
@@ -17,6 +17,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author lwm1435@163.com
@@ -71,19 +72,30 @@ public class KuaiQianController {
 
 
     /**
-     * 处理异步通知
+     * 接收异步通知 处理充值订单
      */
     @GetMapping("/receive/kq/notify")
     @ResponseBody
     public String receiveKqNotify(HttpServletRequest request) {
         //接收快钱步通知消息，处理订单
-        if (kuaiQianService.doKqNotify(request)){
+        kuaiQianService.doKqNotify(request);
             //订单处理完成(1支付完成2没有支付)删除redis中保留的订单
             redisTemplate.opsForZSet().remove(RedisKey.RECHARGE_ORDER_ID_ZSET,
                     request.getParameter("orderId"));
-        }
-
-
         return "<result>1</result><redirecturl>http://localhost:7000/user/recharge/showResult</redirecturl>";
+    }
+
+    /**
+     * 定时任务的接口，向快钱查询充值订单并处理
+     */
+    @GetMapping("/receive/task/kq/query")
+    @ResponseBody
+    public void receiveMicrTask(){
+       //从redis中获取订单集合
+        Set<String> set = redisTemplate.opsForZSet().range(RedisKey.RECHARGE_ORDER_ID_ZSET, 0, -1);
+        //向快钱查询充值订单并处理
+        kuaiQianService.doKqQueryOrder(set);
+
+        System.out.println("订单处理完成");
     }
 }
